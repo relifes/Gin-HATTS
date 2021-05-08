d6e00f6bb1065899775e405d9fb584c3a64ae640
ReLifes <1486176948@qq.com>
2021-05-08 23:53:06 +0800

    解决axios拿不到data的

--- controller/UserController.go
+++ controller/UserController.go
@@ -5,12 +5,20 @@ import (
 	"Gin-HATTS/dto"
 	"Gin-HATTS/model"
 	"Gin-HATTS/response"
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
-	"log"
-	"net/http"
 )
+
+type RequestUser struct {
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+}
 
 // 验证用户是否存在
 func isEmailExist(db *gorm.DB, email string) bool {
@@ -25,9 +33,20 @@ func isEmailExist(db *gorm.DB, email string) bool {
 func Register(ctx *gin.Context) {
 	db := common.GetDB()
 	//获取参数
-	name := ctx.PostForm("name")
-	email := ctx.PostForm("email")
-	password := ctx.PostForm("password")
+	var requestUser RequestUser
+	// var requestUser model.User
+	ctx.Bind(&requestUser)
+	fmt.Println(requestUser)
+
+	name := requestUser.Name
+	email := requestUser.Email
+	password := requestUser.Password
+	fmt.Println(name, email, password)
+
+	if len(email) == 0 || len(name) == 0 || len(password) == 0 {
+		response.Response(ctx, 422, 422, nil, "User 为空")
+		return
+	}
 
 	//判断账号是否存在
 	if isEmailExist(db, email) {
@@ -65,7 +84,7 @@ func Login(ctx *gin.Context) {
 		return
 	}
 	// 判断密码是否正确
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)) ; err != nil {
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
 		response.Response(ctx, http.StatusBadRequest, 400, nil, "密码错误")
 		return
 	}
@@ -76,7 +95,7 @@ func Login(ctx *gin.Context) {
 		return
 	}
 	//返回结果
-	response.Success(ctx, gin.H{"token":token}, "登录成功")
+	response.Success(ctx, gin.H{"token": token}, "登录成功")
 }
 
 func Info(ctx *gin.Context) {
